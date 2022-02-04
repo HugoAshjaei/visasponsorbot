738c846f27a0aee365ee2786b93704884bda38de
Hossein MohammadiPour <me@hossein.link>
2022-02-04 15:33:45 +0330

    hashtags updated for #

--- src/Bots/Telegram/msg.js
+++ src/Bots/Telegram/msg.js
@@ -17,7 +17,7 @@ ${content}
 <a href="${url}"> Read more and apply at ${source}</a>
 
  ${hashtags.map(tag => {
-     tag = tag.replace(/\s+/g, '_').replace(/\./g, '').replace(/\//g, '_').replace(/\(/g, '').replace(/\)/g, '').replace(/\:/g, '').replace(/\,/g, '').replace(/\;/g, '').replace(/\-/g, '_');
+     tag = tag.replace(/\s+/g, '_').replace(/\./g, '').replace(/\//g, '_').replace(/\(/g, '').replace(/\)/g, '').replace(/\:/g, '').replace(/\,/g, '').replace(/\;/g, '').replace(/\-/g, '_').replace(/\#/g, 'sharp')
      return `#${tag}`
     }).join(' ')}
  <a href="https://t.me/visasponsor">@VisaSponsor</a>
