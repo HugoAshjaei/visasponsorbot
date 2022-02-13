abe53131e2029183c77e9afd1b65066a1a7e41bf
Ali Sharafi <a.sharafi1993@gmail.com>
2022-02-13 19:30:04 +0330

    Change telegram message format

--- src/Bots/Telegram/DoStuff.js
+++ src/Bots/Telegram/DoStuff.js
@@ -1,15 +1,29 @@
 const { bot } = require('./bot')
 require('dotenv').config({ path: '../../env' })
-const { creator } = require('./msg')
+const { creator } = require('./msg');
+const MAX_MSG_LENGTH = 3500;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+async function sendMessage(message) {
+    message += `<a href="https://t.me/visasponsor">@VisaSponsor</a>`
+
+    await bot.telegram.sendMessage(process.env.CHANNEL_ID, message, { parse_mode: 'HTML', disable_notification: true })
+}
+
 module.exports.SendJobs = async (data) => {
+    let message = ''
     for (let index = 0; index < data.length; index++) {
-        await bot.telegram.sendMessage(process.env.CHANNEL_ID, creator(data[index]), { parse_mode: 'HTML', disable_notification: true })
-        await sleep(parseInt(process.env.TIME_TO_SEND))
+        message += creator(data[index], index);
+        if (message.length >= MAX_MSG_LENGTH) {
+            await sendMessage(message);
+            message = '';
+            await sleep(process.env.TIME_TO_SEND)
+        }
     }
+
+    if (message.length > 0) sendMessage(message);
 }
 
